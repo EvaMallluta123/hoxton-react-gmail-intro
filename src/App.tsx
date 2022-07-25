cea7830b8e82808c32b486077431c865a9ca9555
Eva Malluta <eva.malluta14@gmail.com>
2022-07-25 23:23:29 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./components/Header";
+import LeftMenu from "./components/LeftMenu";
+import Main from "./components/Main";
+
+function App(): JSX.Element {
+  return (
+    <div className="app">
+      <Header/>
+      <LeftMenu/>
+     <Main/>
+    </div>
+  );
+}
+
+export default App;
